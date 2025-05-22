86ea3d073422c6d4fa93a8aec94b3df5e2438246
Dong <jeedong97@gmail.com>
2025-05-22 10:33:20 +0900

    findObjectWithTag

--- Assets/02.Script/StudyComponent.cs
+++ Assets/02.Script/StudyComponent.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class StudyComponent : MonoBehaviour
 {
-    public GameObject Obj; // 큐브 게임 오브젝트를 가져오고 싶다 
+    public GameObject obj;
 
-    public string changeName;
     void Start()
     {
-        Obj= GameObject.Find("Main Camera");    
+        // Player라는 Tag를 가진 게임오브젝트를 찾아서 obj에 할당
+        obj = GameObject.FindGameObjectWithTag("Player");
+
+        Debug.Log($"이름 : {obj.name}"); // 게임오브젝트의 이름
+        Debug.Log($"태그 : {obj.tag}"); // 게임오브젝트의 태그
+        Debug.Log($"위치 : {obj.transform.position}"); // 게임오브젝트의 Transform 컴포넌트의 위치
+        Debug.Log($"회전 : {obj.transform.rotation}"); // 게임오브젝트의 Transform 컴포넌트의 회전
+        Debug.Log($"크기 : {obj.transform.localScale}"); // 게임오브젝트의 Transform 컴포넌트의 크기
+
     }
+
 }

b624ddddba3d5961fea7000fdc9f663b04c38156
Dong <jeedong97@gmail.com>
2025-05-22 13:10:42 +0900

    [ADD] CreateMethod_createCube

--- Assets/02.Script/StudyComponent.cs
+++ Assets/02.Script/StudyComponent.cs
@@ -4,20 +4,25 @@ public class StudyComponent : MonoBehaviour
 {
     public GameObject obj;
 
+    public Mesh msh;
+    public Material mat;
+
     void Start()
     {
-        // Player라는 Tag를 가진 게임오브젝트를 찾아서 obj에 할당
-        obj = GameObject.FindGameObjectWithTag("Player");
+        CreateCube();
+  }
 
-        Debug.Log($"이름 : {obj.name}"); // 게임오브젝트의 이름
-        Debug.Log($"태그 : {obj.tag}"); // 게임오브젝트의 태그
-        Debug.Log($"위치 : {obj.transform.position}"); // 게임오브젝트의 Transform 컴포넌트의 위치
-        Debug.Log($"회전 : {obj.transform.rotation}"); // 게임오브젝트의 Transform 컴포넌트의 회전
-        Debug.Log($"크기 : {obj.transform.localScale}"); // 게임오브젝트의 Transform 컴포넌트의 크기
+    public void CreateCube()
+    {
+        obj = new GameObject("Cube");
 
-        Debug.Log($"Mesh 데이터 : {obj.GetComponent<MeshFilter>().mesh}");
-        Debug.Log($"Material 데이터 : {obj.GetComponent<MeshRenderer>().material}");
-        obj.GetComponent<MeshRenderer>().enabled = false;
+        obj.AddComponent<MeshFilter>();
+        obj.GetComponent<MeshFilter>().mesh = msh;
+
+        obj.AddComponent<MeshRenderer>();
+        obj.GetComponent<MeshRenderer>().material = mat;
+
+        obj.AddComponent<BoxCollider>();
     }
 
 }

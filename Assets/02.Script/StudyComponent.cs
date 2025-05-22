9e00448a2c5d7a5f9bbdca8fdd703e014294b125
Dong <jeedong97@gmail.com>
2025-05-22 10:39:35 +0900

    getComponent

--- Assets/02.Script/StudyComponent.cs
+++ Assets/02.Script/StudyComponent.cs
@@ -15,6 +15,8 @@ public class StudyComponent : MonoBehaviour
         Debug.Log($"회전 : {obj.transform.rotation}"); // 게임오브젝트의 Transform 컴포넌트의 회전
         Debug.Log($"크기 : {obj.transform.localScale}"); // 게임오브젝트의 Transform 컴포넌트의 크기
 
+        Debug.Log($"Mesh 데이터 : {obj.GetComponent<MeshFilter>().mesh}");
+        Debug.Log($"Material 데이터 : {obj.GetComponent<MeshRenderer>().material}");
     }
 
 }
